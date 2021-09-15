b5057f9967bbd8204f25275ea353479ab3dd6aa3
urgensonamlama <urgensonamlama@gmail.com>
2021-09-15 15:44:57 -0400

    commit tree fffd08972b5e45bbeccb4cbaeb4f91b83fb61280

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let multiply;
+let num1=2; 
+let num2=31;
 
-// Write your code here
+multiply= num1*num2;
+
+let random;
+random= Math.ceil(Math.random()*10)+1;
+
+let mod;
+ let num3=24; 
+ let num4=5;
+
+mod= num3%num4;
+
+let max;
+max = Math.max(2,3,4,11,10,20);
